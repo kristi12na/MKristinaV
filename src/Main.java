a0f2c7c3059ee6e3f2a287fffd1b455d062d8ab3
kristina <kristina_mandruk@mail.ru>
2023-05-09 20:50:46 +0300

    create scanner

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int[][] array = {{7, 3, 2}, {6, 3, 9}};
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int summ = 0;
         System.out.println(Arrays.toString(array));
     }
 }
