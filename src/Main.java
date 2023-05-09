06b2a8a8bac36581f41e93acaf27d9856fec9866
kristina <kristina_mandruk@mail.ru>
2023-05-09 20:48:55 +0300

    create array and fill it with numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] array = {{7, 3, 2}, {6, 3, 9}};
+        System.out.println(Arrays.toString(array));
     }
 }
