1a6e08fce63fed403ce11d17f5167efdca2207aa
kristina <kristina_mandruk@mail.ru>
2023-05-24 18:04:50 +0300

    finish

---
+++ src/Lesson9HW/Main.java
@@ -0,0 +1,10 @@
+package Lesson9HW;
+
+public class Main {
+    public static void main(String[] args) {
+        Figure[] figure = {new Rectangle(8, 12),
+                new Triangle(5, 8, 5),
+                new Circle(10)};
+
+    }
+}
